fcb6d3dc7549dd07a6c63be80672778407260f05
rorrorojas3 <rorro.irg@gmail.com>
2020-07-06 20:56:51 -0400

    Cors
    Added/Implemented CORS

--- net-core-api-boiler-plate/Extensions/Services/CorsService.cs
+++ net-core-api-boiler-plate/Extensions/Services/CorsService.cs
@@ -4,9 +4,18 @@ namespace net_core_api_boiler_plate.Extensions.Services
 {
     public static class CorsService
     {
+        private static readonly string _defaultCorsPolicy = "DefaultCorsPolicy";
+
         public static void AddCorsService(this IServiceCollection services)
         {
-
+            services.AddCors(x => x.AddPolicy(_defaultCorsPolicy,
+                options =>
+                {
+                    options.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader()
+                ;
+                }));
         }
     }
 }
