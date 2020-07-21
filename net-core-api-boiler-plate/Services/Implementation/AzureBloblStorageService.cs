b7ee81b58beadc6be40e060456c62d9da89cae9d
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2020-07-20 23:40:40 -0400

    AzureBlobStorage
    Created Class and interface

---
+++ net-core-api-boiler-plate/Services/Implementation/AzureBloblStorageService.cs
@@ -0,0 +1,12 @@
+using net_core_api_boiler_plate.Services.Interface;
+
+namespace net_core_api_boiler_plate.Services.Implementation
+{
+    /// <summary>
+    ///     AzureBlobStorageService class with Interface implementation
+    /// </summary>
+    public class AzureBloblStorageService : IAzureBlobStorageService
+    {
+
+    }
+}
---
+++ net-core-api-boiler-plate/Services/Interface/IAzureBlobStorageService.cs
@@ -0,0 +1,10 @@
+namespace net_core_api_boiler_plate.Services.Interface
+{
+    /// <summary>
+    ///     IAzureBlobStorage Interface
+    /// </summary>
+    public interface IAzureBlobStorageService
+    {
+
+    }
+}
