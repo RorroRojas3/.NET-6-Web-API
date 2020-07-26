e5d472ccd875c0511c0aecf106f75090f2eb4919
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2020-07-25 22:51:17 -0400

    SignalR
    SignalR interface and class

---
+++ net-core-api-boiler-plate/Services/Interface/ISignalRService.cs
@@ -0,0 +1,10 @@
+namespace net_core_api_boiler_plate.Services.Interface
+{
+    /// <summary>
+    ///     ISingalRService Interface
+    /// </summary>
+    public interface ISignalRService
+    {
+
+    }
+}
