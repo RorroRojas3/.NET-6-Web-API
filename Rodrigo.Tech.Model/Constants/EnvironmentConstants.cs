5d7ce217d4a8abc7ba74a70d464e43217950a4d6
Rodrigo Rojas Garcia <rorro.irg@gmail.com>
2021-01-02 23:47:16 -0500

    Added environment varaibles

--- Rodrigo.Tech.Model/Constants/EnvironmentConstants.cs
+++ Rodrigo.Tech.Model/Constants/EnvironmentConstants.cs
@@ -11,5 +11,15 @@
         public const string COSMOS_KEY = nameof(COSMOS_KEY);
 
         public const string APPINSIGHTS_INSTRUMENTATIONKEY = nameof(APPINSIGHTS_INSTRUMENTATIONKEY);
+
+        public const string CLIENT_ID = nameof(CLIENT_ID);
+
+        public const string CLIENT_SECRET = nameof(CLIENT_SECRET);
+
+        public const string INSTANCE = nameof(INSTANCE);
+
+        public const string TENANT_ID = nameof(TENANT_ID);
+
+        public const string OAUTH2_AUTHORIZATION = nameof(OAUTH2_AUTHORIZATION);
     }
 }
