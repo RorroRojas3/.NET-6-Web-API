ce9035200a03e69b2d6686b4d6c2cafcb91f03d6
Rodrigo Ignacio Rojas Garcia <rorro.irg@gmail.com>
2022-05-03 19:34:26 -0400

    Added middleware exception handler

--- Rodrigo.Tech.Api/Program.cs
+++ Rodrigo.Tech.Api/Program.cs
@@ -1,7 +1,10 @@
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.EntityFrameworkCore;
+using Rodrigo.Rojas.Models.Exceptions;
 using Rodrigo.Rojas.Models.Mappers;
 using Rodrigo.Rojas.Repository.Context;
 using Rodrigo.Rojas.Services;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +27,33 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseExceptionHandler(options =>
+{
+    options.Run(async context =>
+    {
+        var exceptionHandlerPathFeature =
+                context.Features.Get<IExceptionHandlerPathFeature>();
+        var exception = exceptionHandlerPathFeature.Error;
+        if (exception is NotFoundException)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+        }
+        else if (exception is BadRequestException)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        }
+        else if (exception is ConflictException)
+        {
+            context.Response.StatusCode = StatusCodes.Status409Conflict;
+        }
+        else
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        }
+        await context.Response.WriteAsJsonAsync(new {Error = exception.Message});
+    });
+});
 
 app.UseHttpsRedirection();
 
